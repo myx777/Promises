206daf1ad83bde38b3db0fd6f2f77db75b14412f
MYX <it.anton22@gmail.com>
2023-12-15 08:51:53 +0300

    correct

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -1,16 +1,30 @@
 import GameSavingLoader from '../app';
 
-test('should load game saving data successfully', async () => {
-  const saving = await GameSavingLoader.load();
+jest.setTimeout(15000);
 
-  expect(saving).toEqual({
-    id: 9,
-    created: 1546300800,
-    userInfo: {
-      id: 1,
-      name: 'Hitman',
-      level: 10,
-      points: 2000,
-    },
+describe('GameSavingLoader', () => {
+  test('should load game saving data successfully', async () => {
+    const saving = await GameSavingLoader.load();
+  
+    expect(saving).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000,
+      },
+    });
+  }, 10000);
+
+  test('should catch err', async () => {
+    GameSavingLoader.load = jest.fn().mockRejectedValue(new Error('Test error'));
+    try {
+      await GameSavingLoader.load();
+    } catch (error) {
+      expect(error.message).toBe('Test error');
+    }
+    }, 10000);
+
   });
-});
